0f7971d455d790b6633c5bf6ebfc2c40c6d42657
Jetstream <bigalex934@gmail.com>
2020-05-06 17:14:01 +0300

    Create base window for user interaction handling

---
+++ PackmanGame/MainWindow.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_WINDOW_
+#define _MAIN_WINDOW_
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+// Public declarations
+public:
+    explicit MainWindow(QMainWindow * parent = nullptr);
+    //private declarations
+private:
+
+};
+
+
+#endif
